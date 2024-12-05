be192392260d865e8fa44241a3cedf48f8db3c21
Ayush-77-77 <ayush95758@gmail.com>
2024-12-05 17:18:37 +0530

    created FindDuplicateElements file

---
+++ Day-1/FindDuplicateElements.js
@@ -0,0 +1,11 @@
+// 2. Find Duplicate Elements in an Array
+// Objective:
+// To create a program that identifies duplicate elements in an array.
+
+// Program Explanation:
+// The program uses a Set to track unique elements and another Set to store duplicates. By iterating through the array, it adds duplicates to the second set when an element has already been seen.
+
+// Output:
+// The program outputs an array of all duplicate elements.
+
+
