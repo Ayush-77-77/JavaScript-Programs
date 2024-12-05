7f71c0088596da9c4abd58160dc9110604114eac
Ayush-77-77 <ayush95758@gmail.com>
2024-12-05 18:20:58 +0530

    create a file named LongestWordInSentence

---
+++ Day-1/LongesWordInString.js
@@ -0,0 +1,6 @@
+// 4. Find the Longest Word in a Sentence
+// Objective:
+// To create a program that finds the longest word in a given sentence.
+
+// Program Explanation:
+// The program splits the sentence into words and iterates through them, comparing their lengths to find the longest word. If two words have the same length, the program returns the first one it encounters.
