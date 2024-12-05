166fe24b2605ba372808a8bb4e5c221f65cd487c
Ayush-77-77 <ayush95758@gmail.com>
2024-12-05 18:53:26 +0530

    developed a logic to check if the string is palindrome or not, also handled the edge case, file name : StringPalindrome

---
+++ Day-1/StringPalindrome.js
@@ -0,0 +1,50 @@
+// Program Explanation:
+// The program removes non-alphanumeric characters and converts the string to lowercase. It then compares the cleaned string with its reverse to determine if it is a palindrome.
+
+// Output:
+// The program outputs true if the string is a palindrome and false otherwise.
+
+
+function palindromeCheck(alphabetString) {
+
+    // checking string palindrome using two pointer approach
+
+    if (alphabetString.length == 0) {
+        console.log("String is empty");
+        return;
+    }
+    let start = 0;
+    let end = alphabetString.length - 1;
+    let isPalindrome = true;
+    while (start < end) {
+        if (alphabetString[start] != alphabetString[end]) {
+            isPalindrome = false;
+        }
+        start++;
+        end--;
+    }
+    if (isPalindrome) {
+        console.log("String is Palindrome");
+    }
+    else {
+        console.log("String is not palindrome");
+    }
+
+}
+
+
+function ignoreNumeric(sentence) {
+    let alphabetString = ""
+    sentence = sentence.toLowerCase();
+    for (let i = 0; i < sentence.length; i++) {
+        if ((sentence[i] >= 'a' && sentence[i] <= 'z')) {
+            alphabetString = alphabetString + sentence[i];
+        }
+    }
+    palindromeCheck(alphabetString);
+
+}
+
+
+let sentence = "52626"
+ignoreNumeric(sentence)
