36cb9bfd01a0ed6cc65a2766145fd8025354326a
Ayush-77-77 <ayush95758@gmail.com>
2024-12-06 17:20:36 +0530

    written a logic to check palindrome of a string using recursion

--- RecursivePalindrome.js
+++ RecursivePalindrome.js
@@ -1,2 +1,24 @@
 // Program to check if string is palindrome using recursion
 
+function stringPalindrome(string, index) {
+
+    if (string === "") {
+        return false;
+    }
+    if (index >= string.length / 2) {
+        return true;
+    }
+
+
+    if (string[index] !== string[string.length - 1 - index]) {
+        return false;
+    }
+    return stringPalindrome(string, index + 1);
+
+
+
+
+}
+
+
+console.log(stringPalindrome("MAAADAAA1M", 0));
